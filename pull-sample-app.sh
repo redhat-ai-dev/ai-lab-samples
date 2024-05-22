dc910962843c2c254d4f2a34283d69208708e77a
Stephanie <yangcao@redhat.com>
2024-05-22 15:10:11 -0400

    add script to pull in latest sample
    Signed-off-by: Stephanie <yangcao@redhat.com>

---
+++ pull-sample-app.sh
@@ -0,0 +1,19 @@
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+
+CHATBOT_DIR=$ROOT_DIR/chatbot
+CODEGEN_DIR=$ROOT_DIR/codegen
+
+REPO="https://github.com/containers/ai-lab-recipes"
+
+TEMPDIR=$ROOT_DIR/temp
+rm -rf $TEMPDIR # clean up
+mkdir -p $TEMPDIR
+cd $TEMPDIR
+git clone $REPO 2>&1 > /dev/null
+
+REPONAME=$(basename $REPO)
+
+cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/chatbot/app/ $CHATBOT_DIR/
+cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/codegen/app/ $CODEGEN_DIR/  
+
+rm -rf $TEMPDIR # clean up
