cc5409b3e2c8864bc60f1108195724d3c5e003dd
Maysun J Faisal <maysunaneek@gmail.com>
2024-08-07 19:26:33 -0400

    Update RAG sample and pull latest
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pull-sample-app.sh
+++ pull-sample-app.sh
@@ -4,6 +4,7 @@ CHATBOT_DIR=$ROOT_DIR/chatbot/
 CODEGEN_DIR=$ROOT_DIR/codegen/
 AUDIO_TO_TEXT_DIR=$ROOT_DIR/audio-to-text/
 OBJECTION_DETECTION_DIR=$ROOT_DIR/object-detection/
+RAG_DIR=$ROOT_DIR/rag/
 
 REPO="https://github.com/containers/ai-lab-recipes"
 
@@ -19,5 +20,6 @@ cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/chatbot/app/ $CHATB
 cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/codegen/app/ $CODEGEN_DIR
 cp -r $TEMPDIR/$REPONAME/recipes/audio/audio_to_text/app/ $AUDIO_TO_TEXT_DIR
 cp -r $TEMPDIR/$REPONAME/recipes/computer_vision/object_detection/app/ $OBJECTION_DETECTION_DIR
+cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/rag/app/ $RAG_DIR
 
 rm -rf $TEMPDIR # clean up
